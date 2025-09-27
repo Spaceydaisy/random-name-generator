8f7f95b07ca123136a673ca8b938fc63ddb5125c
Spaceydaisy <117010742+Spaceydaisy@users.noreply.github.com>
2025-09-27 23:02:02 +0530

    Add NameGenerator component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello React!</h1>
-    </div>
-  );
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App"; // 👈 important: no .jsx here, just './App'
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
